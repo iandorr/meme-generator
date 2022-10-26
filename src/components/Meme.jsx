5aa2adc25dd930463275a1c91232914ca7fa4ecd
Damian Siskovic <siskovic.damian@gmail.com>
2022-10-27 00:11:53 +0200

    06_project-get-random-meme; Adds event listener function onClick;

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,35 @@
+import memesData from '../assets/data/memesData'
+
 const Meme = () => {
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomMeme = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomMeme].url
+
+    console.log(url)
+  }
+
   return (
     <main>
-      <form className="form">
-        <input className="form--input" type="text" placeholder="Top text"/>
-        <input className="form--input" type="text" placeholder="Bottom text"/>
-        <button className="form--button">Get a new meme image</button>
-      </form>
+      <div className="form">
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Top text"
+        />
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Bottom text"
+        />
+        <button
+          className="form--button"
+          onClick={getMemeImage}
+        >
+          Get a new meme image
+        </button>
+      </div>
     </main>
   )
 }
